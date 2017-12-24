02dec88b44df64fc9716d86115cfd4453fb93dc4
John Thompson <john@springframework.guru>
2017-12-24 12:19:02 -0500

    creating category controller

---
+++ src/main/java/guru/springframework/spring5webfluxrest/controllers/CategoryController.java
@@ -0,0 +1,33 @@
+package guru.springframework.spring5webfluxrest.controllers;
+
+import guru.springframework.spring5webfluxrest.domain.Category;
+import guru.springframework.spring5webfluxrest.repositories.CategoryRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by jt on 12/24/17.
+ */
+@RestController
+public class CategoryController {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping("/api/v1/categories")
+    Flux<Category> list(){
+        return categoryRepository.findAll();
+    }
+
+    @GetMapping("/api/v1/categories/{id}")
+    Mono<Category> getById(@PathVariable String id){
+        return categoryRepository.findById(id);
+    }
+
+}
